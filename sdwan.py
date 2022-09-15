87e24378b4b396d36d3f9646aaa121fd333b9c4b
Piotr Sumelka <piotr.sumelka@gmail.com>
2022-09-15 12:28:24 +0200

    adding click library

--- sdwan.py
+++ sdwan.py
@@ -2,6 +2,7 @@
 
 import requests
 import tabulate
+import click
 import creds
 
 class Connector:
@@ -15,30 +16,30 @@ class Connector:
         self.get_token()
 
     def get_session_cookie(self):
-        url = self.base_url + "j_security_check"
-        payload = {"j_username": self.username, "j_password": self.password}
+        url = self.base_url + 'j_security_check'
+        payload = {'j_username': self.username, 'j_password': self.password}
 
         response = requests.post(url, data=payload)
         if response.status_code != 200:
-            print("Cannot download a cookie")
+            print('Cannot download a cookie')
             return 0
-        self.session_cookie = response.headers["Set-cookie"].split(";")[0]
+        self.session_cookie = response.headers['Set-cookie'].split(';')[0]
         return 1
 
     def get_token(self):
-        url = self.base_url + "dataservice/client/token"
-        headers = {"Cookie": self.session_cookie}
+        url = self.base_url + 'dataservice/client/token'
+        headers = {'Cookie': self.session_cookie}
 
         response = requests.get(url, headers=headers)
         if response.status_code != 200:
-            print("Cannot get a token")
+            print('Cannot get a token')
             return 0
         self.token = response.text
         return 1
 
     @property
     def headers(self):
-        return {"Cookie": self.session_cookie, "X-XSRF-TOKEN": self.token}
+        return {'Cookie': self.session_cookie, 'X-XSRF-TOKEN': self.token}
     
     def get_data(self, path, params=None):
         url = self.base_url + path
@@ -116,24 +117,53 @@ class Connector:
                     ]
             )
         return devices
-    
-if __name__ == '__main__':
+
+@click.group()
+def cli():
+    pass
+
+@click.command()
+def get_devices():
     conn = Connector(creds.username, creds.password, creds.base_url)
     conn.connect()
 
-    #devices = conn.get_devices()
-    #headers = ['Device ID', 'Name', 'Site-ID', 'System-IP', 'Board-serial', 'Model', 'Software version']
-    #print(tabulate.tabulate(devices, headers, tablefmt='fancy_grid'))
+    devices = conn.get_devices()
+    headers = ['Device ID', 'Name', 'Site-ID', 'System-IP', 'Board-serial', 'Model', 'Software version']
+    print(tabulate.tabulate(devices, headers, tablefmt='fancy_grid'))
 
-    #templates = conn.get_templates()
-    #headers = [""Template ID", Template Name", "Device Type","Attached devices", "Template version"]
-    #print(tabulate.tabulate(templates, headers, tablefmt="fancy_grid"))
+@click.command()
+def get_templates():
+    conn = Connector(creds.username, creds.password, creds.base_url)
+    conn.connect()
 
-    #features = conn.get_features()
-    #headers = ['Template ID', 'Template Name', 'Template Type']
-    #print(tabulate.tabulate(features, headers, tablefmt='fancy_grid'))
+    templates = conn.get_templates()
+    headers = ['Template ID', 'Template Name', 'Device Type','Attached devices', 'Template version']
+    print(tabulate.tabulate(templates, headers, tablefmt='fancy_grid'))
 
-    template_id = 'b7ccd31e-0e59-417d-8f52-72fc9106060a'
-    attached_devices = conn.get_devices_by_template(template_id)
+@click.command()
+def get_features():
+    conn = Connector(creds.username, creds.password, creds.base_url)
+    conn.connect()
+
+    features = conn.get_features()
+    headers = ['Template ID', 'Template Name', 'Template Type']
+    print(tabulate.tabulate(features, headers, tablefmt='fancy_grid'))
+
+
+@click.command()
+@click.option('-t', '--template', type=str, required=True, help='Template ID')
+def get_devices_by_template(template):
+    conn = Connector(creds.username, creds.password, creds.base_url)
+    conn.connect()
+
+    attached_devices = conn.get_devices_by_template(template)
     headers = ['UUID', 'Name', 'IP', 'Site-ID']
     print(tabulate.tabulate(attached_devices, headers, tablefmt='fancy_grid'))
+
+cli.add_command(get_devices)
+cli.add_command(get_templates)
+cli.add_command(get_features)
+cli.add_command(get_devices_by_template)
+
+if __name__ == '__main__':
+    cli()
